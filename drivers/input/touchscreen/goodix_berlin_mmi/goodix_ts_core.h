6d3fa990ef7199b7d499aafa071f2a891e215c9e
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-05-07 10:33:50 +0530

    input: ts: goodix_berlin_mmi: stfu

--- drivers/input/touchscreen/goodix_berlin_mmi/goodix_ts_core.h
+++ drivers/input/touchscreen/goodix_berlin_mmi/goodix_ts_core.h
@@ -629,7 +629,7 @@ static struct goodix_ext_attribute ext_attr_##_name = \
 
 /* log macro */
 extern bool debug_log_flag;
-#define ts_info(fmt, arg...)	pr_info("[GTP-INF][%s:%d] "fmt"\n", __func__, __LINE__, ##arg)
+#define ts_info(fmt, arg...)	pr_debug("[GTP-INF][%s:%d] "fmt"\n", __func__, __LINE__, ##arg)
 #define	ts_err(fmt, arg...)		pr_err("[GTP-ERR][%s:%d] "fmt"\n", __func__, __LINE__, ##arg)
 #define ts_debug(fmt, arg...)	{if (debug_log_flag) pr_info("[GTP-DBG][%s:%d] "fmt"\n", __func__, __LINE__, ##arg);}
 
